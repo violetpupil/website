a05d16d17710dd50d043ae79668cbf0afc099f87
Shouguang Zhang <495140158@qq.com>
2021-11-16 14:03:54 +0800

    index

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <h1>Hello, world!</h1>
   </React.StrictMode>,
   document.getElementById('root')
 );
