86306f63e7ae2076eda8cdca7b624ad8f48fb028
Shouguang Zhang <495140158@qq.com>
2021-11-28 23:07:04 +0800

    app

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,17 @@ import Search from "./components/Search"
 import Bar from "./components/Bar"
 
 export default class App extends Component {
+  state = {
+    bar: ""
+  }
+
+  updateAppState = state => this.setState(state)
+
   render() {
     return (
       <>
-        <Search />
-        <Bar />
+        <Search updateAppState={this.updateAppState} />
+        <Bar {...this.state} />
       </>
     )
   }
