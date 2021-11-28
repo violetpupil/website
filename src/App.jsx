52d9397ffd5efa645128fbee196d7f75e20a8e3f
Shouguang Zhang <495140158@qq.com>
2021-11-28 21:54:56 +0800

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
+import Search from "./components/Search"
+import Bar from "./components/Bar"
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-
-      </div>
+      <>
+        <Search />
+        <Bar />
+      </>
     )
   }
 }
