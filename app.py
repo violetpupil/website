35ad5fc31b4eb7cf97482923e270ce2f957e5b19
Shouguang Zhang <495140158@qq.com>
2021-11-28 12:10:57 +0800

    temp

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
+import pygal
+import requests
 from fastapi import FastAPI
+from pygal.style import LightenStyle, LightColorizedStyle
 
 app = FastAPI()
 
@@ -6,3 +9,36 @@ app = FastAPI()
 @app.get("/popular")
 def popular():
     return "index"
+
+
+def temp(q):
+    res = requests.get(f"https://api.github.com/search/repositories?q=language:{q}&sort=stars")
+    if res.status_code != 200:
+        return f"{res.status_code} {res.reason} {res.text}"
+
+    names, plot_dicts = [], []
+    for item in res.json().get('items'):
+        names.append(item.get('name'))
+        plot_dicts.append({
+            'value': item.get('stargazers_count'),
+            'label': item.get('description'),
+            'xlink': item.get('html_url')
+        })
+
+    config = pygal.Config()
+    config.x_label_rotation = 45
+    config.show_legend = False
+    config.truncate_label = 15
+    config.show_y_guides = False
+    config.width = 1000
+    chart = pygal.Bar(config, style=LightenStyle(
+        '#333366',
+        base_style=LightColorizedStyle,
+        title_font_size=24,
+        label_font_size=14,
+        major_label_font_size=18,
+    ))
+    chart.title = "Most-Starred Python Projects on GitHub"
+    chart.x_labels = names
+    chart.add('', plot_dicts)
+    chart.render_to_file('python_repos.svg')
