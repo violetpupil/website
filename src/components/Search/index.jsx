bbb122e09526c716431e3315c74dbda6db99c7b4
Shouguang Zhang <495140158@qq.com>
2021-11-28 23:12:25 +0800

    search

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
 
 export default class Search extends Component {
+  inputRef = React.createRef()
+
+  search = async () => {
+    const res = await fetch(`/popular?q=${this.inputRef.current.value}`)
+    this.props.updateAppState({ bar: await res.text() })
+  }
+
   render() {
     return (
       <div>
-
+        <input ref={this.inputRef} type="text"></input>
+        <button onClick={this.search} >search</button>
       </div>
     )
   }
