b546339b6a796fb27bff46373ce1c380caab5e45
Shouguang Zhang <495140158@qq.com>
2021-11-29 10:41:21 +0800

    clear

--- src/components/Bar/index.jsx
+++ src/components/Bar/index.jsx
@@ -4,14 +4,10 @@ export default class Bar extends Component {
   render() {
     const { bar, isFirst, isLoading, err } = this.props
     return (
-      <div>
-        {
-          isFirst ? "" :
-            isLoading ? <h2>Loading......</h2> :
-              err ? <h2>{err}</h2> :
-                <div dangerouslySetInnerHTML={{ __html: bar }} />
-        }
-      </div>
+      isFirst ? "" :
+        isLoading ? <h2>Loading......</h2> :
+          err ? <h2>{err}</h2> :
+            <div dangerouslySetInnerHTML={{ __html: bar }} />
     )
   }
 }
